f77d43c5811c5982cc60b872c93078f263d98cc0
Rvasavi96 <rapuru.vasavi246@gmail.com>
2024-02-15 16:26:14 +0530

    hello world script i have leaoporned

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "hello world,i am learning devops"
+PERSON1=Ramesh
+PERSON2=Suresh
+
+eccho "$PERSON1: Hi $PERSON1, Good Morning"
+echo "$PERSON1:Good Morning $PERSON1"
+eccho "$PERSON1: Hi $PERSON1, Good00 Morning"
+echo "$PERSON1:Goo000d Morning $PERSON1"
